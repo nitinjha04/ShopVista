142e7ef9e37cba3121a516e9be444c72985f74ac
nitin <nitinjha890@gmail.com>
2023-12-23 23:15:00 +0530

    minor changes

--- services/common.js
+++ services/common.js
@@ -1,21 +1,3 @@
-// // const passport = require("passport");
-
-// // exports.isAuth = (req, res, done) => {
-// //   return passport.authenticate("jwt");
-// // };
-
-// // exports.sanitizeUser = (user) => {
-// //   return { id: user.id, role: user.role };
-// // };
-
-// // exports.cookieExtractor = function (req, res, next) {
-// //   let token = null;
-// //   if (req && req.cookies) {
-// //     token = req.cookies["jwt"];
-// //   }
-// //   return token;
-// // };
-
 const passport = require("passport");
 
 const isAuth = (req, res, done) => {
@@ -31,31 +13,8 @@ const cookieExtractor = function (req, res, next) {
   if (req && req.cookies) {
     token = req.cookies["jwt"];
   }
-  // token =
-  // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODFlNTBlNmM1ZGNhMjI1NWViODNiYSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzAzMDExNTk4fQ.AyzGq_Nn0fXDgNoIuSGqU0hEHWQTrA3w7N5MzulUNjM"
 
   return token;
 };
 
 module.exports = { isAuth, sanitizeUser, cookieExtractor };
-
-// const passport = require("passport");
-
-// exports.isAuth = (req, res, done) => {
-//   return passport.authenticate("jwt");
-// };
-
-// exports.sanitizeUser = (user) => {
-//   return { id: user.id, role: user.role };
-// };
-
-// exports.cookieExtractor = function (req, res, next) {
-//   let token = null;
-//   if (req && req.cookies) {
-//     token = req.cookies["jwt"];
-//   }
-  
-//   token =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODFlNTBlNmM1ZGNhMjI1NWViODNiYSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzAzMDExNTk4fQ.AyzGq_Nn0fXDgNoIuSGqU0hEHWQTrA3w7N5MzulUNjM"
-//   return token;
-// };
